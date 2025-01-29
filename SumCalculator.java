a9829c69bf068981215a6d4e65cc020fd36da141
Tanneeshha <126338977+Tanneeshha@users.noreply.github.com>
2025-01-29 13:32:17 +0500

    Create SumCalculator.java

---
+++ SumCalculator.java
@@ -0,0 +1,10 @@
+// Task 1: Program to calculate the sum of two numbers
+public class SumCalculator {
+    public static void main(String[] args) {
+        System.out.println("This program has calculated the sum of two numbers:");
+        int num1 = 9;
+        int num2 = 67;
+        int sum = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+    }
+}
